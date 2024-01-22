3d4bad251c82aca05dca8b780eb69665034cd6d4
Pedro Serio <pedroaserio@gmail.com>
2024-01-22 19:52:13 -0300

    Initial commit

---
+++ R/fun_one.R
@@ -0,0 +1,13 @@
+#' Simples function to sum up two numbers
+#'
+#' @param num1 a numeric value
+#' @param num2 another numeric value
+#'
+#' @return the sum of two numeric values
+#' @export
+#'
+#' @examples
+#' myfunc1(1+2)
+myfunc1 <- function(num1=numeric(),num2=numeric()) {
+  return(num1+num2)
+}
